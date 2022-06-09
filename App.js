f0dc98e69455d010c7df7f0355d7e871eb1cd09c
ahnniething <ahnniething@gmail.com>
2022-06-09 16:54:15 +0900

    refactor: make functions for preloading many fonts and images

--- App.js
+++ App.js
@@ -4,31 +4,35 @@ import { Text, Image } from 'react-native'
 import * as Font from 'expo-font'
 import { Ionicons } from '@expo/vector-icons';
 import { Asset } from 'expo-asset';
-  /*
-    만약 앱이 실행될 준비가 되어있지 않으면 AppLoading을 보여줌
-    AppLoading은 splash screen을 띄우고 startAsync 함수를 호출함
-    startAsync 가 완료되면 onFinish 함수를 호출하여 state를 변경함
-    state가 변경되면 'we are done loading!'을 화면에 보여줌
-  */
+
+/**
+ * 함수
+ *
+ */
+const loadFonts = (fonts) => fonts.map(font => Font.loadAsync(font));
+const loadImages = (images) => images.map(image => typeof image === "string" ? Image.prefetch(image) : Asset.loadAsync(image));
+
+/**
+ *  1. 만약 앱이 실행될 준비가 되어있지 않으면 AppLoading을 보여줌
+ *  2. AppLoading은 splash screen을 띄우고 startAsync 함수를 호출함
+ *  3. startAsync 가 완료되면 onFinish 함수를 호출하여 state를 변경함
+ *  4. state가 변경되면 'we are done loading!'을 화면에 보여줌
+ */
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
-  //10초 후에 앱이 실행되도록 한다.
   const startLoading = async () => {
     //10초 뒤에 앱 실행
     // await new Promise(resolve => setTimeout(resolve, 10000));
 
-    //Font
     //preload font
-    await Font.loadAsync(Ionicons.font);
+    const fonts = loadFonts([Ionicons.font]);
 
-    //Image
-    //preload image on my local file system (better way)
-    await Asset.loadAsync(require('./cdd.png'));
-     //preload image on server (not recommended)
-     await Image.prefetch('https://d33wubrfki0l68.cloudfront.net/b152eb4214943f96e83c4babde026b12221e68f1/a20c2/img/oss_logo.png');
-
+    //preload image
+    const images = loadImages([require('./cdd.png'), 'https://d33wubrfki0l68.cloudfront.net/b152eb4214943f96e83c4babde026b12221e68f1/a20c2/img/oss_logo.png']);
+    await Promise.all([...fonts, ...images]);
   };
+
   if(!ready){
     return <AppLoading
     startAsync={startLoading}
