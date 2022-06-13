536be2f615e8d5da501c4da745f60a434e89c800
ahnniething <ahnniething@gmail.com>
2022-06-13 10:40:32 +0900

    feat: add tab navigation using react-navigation

--- App.js
+++ App.js
@@ -5,18 +5,20 @@ import * as Font from 'expo-font'
 import { Ionicons } from '@expo/vector-icons';
 import { Asset, useAssets } from 'expo-asset';
 import { useFonts } from 'expo-font';
-
+import { NavigationContainer } from '@react-navigation/native';
+import Tabs from './navigation/Tabs';
 /**
  *  이미지와 폰트가 불러와지지 않았다면 <AppLoading>을 리턴함
  */
 export default function App() {
   const [assets] = useAssets([require("./cdd.png")]);
   const [loaded] = Font.useFonts(Ionicons.font);
-  if(!assets||!!loaded){
+
+  if(!assets||!loaded){
     return <AppLoading/>;
   }
-  //앱이 실행될 준비가 완료되었을 때(=이미지와 폰트가 불러와졌을 때) 화면에 'we are done loading!'을 보여줌
-  return <Text>we are done loading!</Text>;
-
+  return <NavigationContainer>
+    <Tabs/>
+  </NavigationContainer>
 }
 
