39af3dc1b0890e77a404a746657e20b5f2e7c09c
ahnniething <ahnniething@gmail.com>
2022-06-13 12:07:06 +0900

    feat: configure navigation by using screen options and options

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,17 +1,41 @@
-import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
 import Movies from "../screens/Movies";
-import Tv from "../screens/Tv";
 import Search from "../screens/Search";
+import Tv from "../screens/Tv";
+import { View, Text } from "react-native";
+
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-<Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies}/>
-    <Tab.Screen name="Tv" component={Tv}/>
-    <Tab.Screen name="Search" component={Search}/>
-</Tab.Navigator>
+  <Tab.Navigator
+    initialRouteName="Tv"
+    screenOptions={{
+      tabBarStyle: { backgroundColor: "lavender" },
+      tabBarLabelStyle: {
+        backgroundColor: "pink",
+      },
+      tabBarLabelPosition: "below-icon",
+      tabBarActiveTintColor: "magenta",
+    }}
+  >
+    <Tab.Screen
+      name="Movies"
+      component={Movies}
+      options={{
+        tabBarLabelStyle: {
+          backgroundColor: "yellow",
+        },
+      }}
+    />
+    <Tab.Screen name="Tv" component={Tv} options={{ tabBarBadge: 5, tabBarStyle:{backgroundColor:"cyan"}, headerStyle:{backgroundColor:"cyan"}, headerTitleStyle:{color:"tomato"}, headerRight: () => <View><Text>more</Text></View> }} />
+    <Tab.Screen
+      name="Search"
+      component={Search}
+      options={{ tabBarBadge: "click!" }}
+    />
+  </Tab.Navigator>
 );
 
-export default Tabs
+export default Tabs;
