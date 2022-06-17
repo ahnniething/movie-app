c293c512d2618a2b3a6209e29bedf420d7a0f96a
ahnniething <ahnniething@gmail.com>
2022-06-17 12:29:19 +0900

    feat: configure presentiation and  animations of stack navigation props

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,38 +1,45 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Text, View, TouchableOpacity } from "react-native";
+import { YELLOW_COLOR } from "../colors";
 
 const ScreenOne = ({ navigation: { navigate } }) => (
   // <NativeStack.Screen name="Two" component={ScreenTwo} />의 "Two" = navigate("Two")}
   // screen name을 파라미터로 보냄
-  <TouchableOpacity onPress={() => navigate("Two")} >
-  <View>
-    <Text>go to two</Text>
-  </View>
+  <TouchableOpacity onPress={() => navigate("Two")}>
+    <View>
+      <Text>go to two</Text>
+    </View>
   </TouchableOpacity>
 );
 const ScreenTwo = ({ navigation: { navigate } }) => (
-  <TouchableOpacity onPress={() => navigate("Three")} >
-  <View>
-    <Text>go to three</Text>
-  </View>
+  <TouchableOpacity onPress={() => navigate("Three")}>
+    <View>
+      <Text>go to three</Text>
+    </View>
   </TouchableOpacity>
 );
 const ScreenThree = ({ navigation: { setOptions } }) => (
-  <TouchableOpacity onPress={() => setOptions({title: "Hello"})} >
-  <View>
-    <Text>Change title</Text>
-  </View>
+  <TouchableOpacity onPress={() => setOptions({ title: "Hello" })}>
+    <View>
+      <Text>Change title</Text>
+    </View>
   </TouchableOpacity>
 );
 
 const NativeStack = createNativeStackNavigator();
 
 const Stack = () => (
-  <NativeStack.Navigator>
+  <NativeStack.Navigator
+    screenOptions={{
+      animation:"flip",
+      headerTintColor: YELLOW_COLOR,
+      headerBackTitleVisible: false,
+    }}
+  >
     <NativeStack.Screen name="One" component={ScreenOne} />
     <NativeStack.Screen name="Two" component={ScreenTwo} />
-    <NativeStack.Screen name="Three" component={ScreenThree} />
+    <NativeStack.Screen name="Three" component={ScreenThree} options={{ presentation:"modal"}}/>
   </NativeStack.Navigator>
 );
 
