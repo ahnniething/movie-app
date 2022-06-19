f69eeed62081d14d9df261df20b6484472d3c80b
ahnniething <ahnniething@gmail.com>
2022-06-19 23:32:28 +0900

    feat: update styles by using styled-components

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,25 +1,22 @@
 import React from "react";
-import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import styled from "styled-components/native";
+
+const Btn = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+`;
+
+const Title = styled.Text`
+  color: blue;
+`;
 
 const Movies = ({ navigation: { navigate } }) => (
-  <TouchableOpacity
-    onPress={() => navigate("Stack", { screen: "Three" })}
-    style={styles.btn}
-  >
-    <Text style={styles.text}>go to Three in Stack Naigator</Text>
-  </TouchableOpacity>
+  <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
+    <Title>go to Three in Stack Naigator</Title>
+  </Btn>
 );
 
-const styles = StyleSheet.create({
-     btn : {
-          flex: 1, 
-          justifyContent: "center", 
-          alignItems: "center" 
-     },
-     text: {  
-          color:"blue"
-     }
-})
 export default Movies;
-
-
