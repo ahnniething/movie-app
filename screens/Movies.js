967df04cdcf3aba2d54d0e2d3184f851028d3fd5
ahnniething <ahnniething@gmail.com>
2022-06-20 09:17:06 +0900

    feat: show different colors depending on the props

--- screens/Movies.js
+++ screens/Movies.js
@@ -6,7 +6,6 @@ const Btn = styled.TouchableOpacity`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: red;
 `;
 
 const Title = styled.Text`
@@ -16,6 +15,7 @@ const Title = styled.Text`
 const Movies = ({ navigation: { navigate } }) => (
       <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
         <Title selected={true} >go to Three in Stack Naigator</Title>
+        <Title selected={false} >go to Three in Stack Naigator</Title>
       </Btn>
 );
 
