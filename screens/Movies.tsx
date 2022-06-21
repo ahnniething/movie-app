399232be572ca61a0d3877cabdcd14cf92d7208d
ahnniething <ahnniething@gmail.com>
2022-06-21 09:47:32 +0900

    #1 feat: add blur to movie  background images

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,9 +1,11 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect, useState } from "react";
-import Swiper from "react-native-web-swiper";
+// import Swiper from "react-native-web-swiper";
+import Swiper from "react-native-swiper";
 import styled from "styled-components/native";
 import { ActivityIndicator, Dimensions } from "react-native";
 import { makeImgPath } from "../utils";
+import { BlurView } from "expo-blur";
 
 const API_KEY = "babd9d44a661b74fe4939712438ad188";
 
@@ -20,8 +22,12 @@ const Loader = styled.View`
 `;
 
 const BgImg = styled.Image`
-  flex: 1;
+  width: 100%;
+  height: 100%;
+  position: absolute;
 `;
+
+const Title = styled.Text``;
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
@@ -50,12 +56,20 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
     <Container>
       <Swiper
         loop
-        timeout={3.5}
+        autoplay={true}
+        autoplayTimeout={3.5}
+        showsPagination={false}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
         {nowPlayingMovies.map((movie) => (
           <View key={movie.id}>
             <BgImg source={{ uri: makeImgPath(movie.backdrop_path) }} />
+            <BlurView
+              intensity={3}
+              style={{ width: "100%", height: "100%", position: "absolute" }}
+            >
+              <Title>{movie.original_title}</Title>
+            </BlurView>
           </View>
         ))}
       </Swiper>
