7d14831df417967ac3601537139e6893d23c7af6
ahnniething <ahnniething@gmail.com>
2022-06-21 15:03:41 +0900

    #3 feat: add a trending movie list using Scroll View and close #3

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,14 +1,22 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, Dimensions, useColorScheme } from "react-native";
+import {
+  ActivityIndicator,
+  Dimensions,
+  ScrollView,
+  useColorScheme,
+} from "react-native";
 // import Swiper from "react-native-web-swiper";
 import Swiper from "react-native-swiper";
 import styled from "styled-components/native";
+import Poster from "../components/Poster";
 import Slide from "../components/Slide";
 
 const API_KEY = "babd9d44a661b74fe4939712438ad188";
 
 const Container = styled.ScrollView``;
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Loader = styled.View`
   flex: 1;
@@ -16,24 +24,75 @@ const Loader = styled.View`
   align-items: center;
 `;
 
-const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+const ListTitle = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  font-size: 18px;
+  font-weight: 600;
+  margin-left: 30px;
+`;
+
+const TrendingScroll = styled.ScrollView`
+  margin-top: 20px;
+`;
+
+const Movie = styled.View`
+  margin-right: 20px;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  font-weight: 600;
+  margin-top: 7px;
+  margin-bottom: 5px;
+`;
+
+const Votes = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  font-size: 10px;
+`;
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
   const isDark = useColorScheme() === "dark";
   const [loading, setLoading] = useState(true);
-  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [trending, setTrending] = useState([]);
+
   const getNowPlaying = async () => {
     const { results } = await (
       await fetch(
         `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
       )
     ).json();
-    setNowPlayingMovies(results);
+    setNowPlaying(results);
+  };
+
+  const getUpcoming = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+      )
+    ).json();
+    setUpcoming(results);
+  };
+
+  const getTrending = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+      )
+    ).json();
+    setTrending(results);
+  };
+
+  const getData = async () => {
+    await Promise.all([getNowPlaying(), getUpcoming(), getTrending()]);
     setLoading(false);
   };
 
   useEffect(() => {
-    getNowPlaying();
+    getData();
   }, []);
 
   return loading ? (
@@ -49,9 +108,13 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
         autoplayTimeout={3.5}
         showsButtons={false}
         showsPagination={false}
-        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+        containerStyle={{
+          marginBottom: 20,
+          width: "100%",
+          height: SCREEN_HEIGHT / 4,
+        }}
       >
-        {nowPlayingMovies.map((movie) => (
+        {nowPlaying.map((movie) => (
           <Slide
             key={movie.id}
             backdropPath={movie.backdrop_path}
@@ -62,6 +125,23 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
           ></Slide>
         ))}
       </Swiper>
+      <ListTitle>Trending Movies</ListTitle>
+      <TrendingScroll
+        contentContainerStyle={{ paddingLeft: 30 }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        {trending.map((movie) => (
+          <Movie key={movie.id}>
+            <Poster path={movie.poster_path} />
+            <Title>
+              {movie.original_title.slice(0, 13)}
+              {movie.original_title.length > 13 && "..."}{" "}
+            </Title>
+            <Votes>⭐️ {movie.vote_average}/10</Votes>
+          </Movie>
+        ))}
+      </TrendingScroll>
     </Container>
   );
 };
